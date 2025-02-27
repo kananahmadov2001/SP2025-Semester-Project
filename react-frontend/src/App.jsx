6801d5cbe55014270a887715cc4ef925061b5773
Weikai Rao <weikairao@gmail.com>
2025-02-26 23:25:47 -0600

    Adjust App routes so proctected pages requires login

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,11 +1,15 @@
 // react-frontend/src/App.jsx
+
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// Import the Layout
-import Layout from "./components/layout/Layout";
+// 1) Import AuthProvider
+import { AuthProvider } from "./context/AuthContext";
+// 2) Import our ProtectedRoute
+import ProtectedRoute from "./components/ProtectedRoute";
 
-// Import all the pages
+// 3) Import the Layout & pages
+import Layout from "./components/layout/Layout";
 import HomePage from "./HomePage";
 import DashboardPage from "./DashboardPage";
 import TeamViewPage from "./TeamViewPage";
@@ -14,22 +18,43 @@ import ChallengePage from "./ChallengePage";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* 
-          Wrap your entire application (or the portion that shares the layout) 
-          in a Route that renders <Layout /> 
-        */}
-        <Route path="/" element={<Layout />}>
-          {/* These are child routes that will render in <Outlet /> inside Layout */}
-          <Route index element={<HomePage />} />
-          <Route path="dashboard" element={<DashboardPage />} />
-          <Route path="dashboard/teamView" element={<TeamViewPage />} />
-          <Route path="dashboard/trashTalk" element={<TrashTalkPage />} />
-          <Route path="dashboard/challenge" element={<ChallengePage />} />
-        </Route>
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            {/* Public routes */}
+            <Route index element={<HomePage />} />
+            <Route path="dashboard" element={<DashboardPage />} />
+
+            {/* Protected routes for logged-in users only */}
+            <Route
+              path="dashboard/teamView"
+              element={
+                <ProtectedRoute>
+                  <TeamViewPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="dashboard/trashTalk"
+              element={
+                <ProtectedRoute>
+                  <TrashTalkPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="dashboard/challenge"
+              element={
+                <ProtectedRoute>
+                  <ChallengePage />
+                </ProtectedRoute>
+              }
+            />
+          </Route>
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
